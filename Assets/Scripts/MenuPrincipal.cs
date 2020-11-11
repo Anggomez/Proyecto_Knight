b102fb2641f6dca25a019059b3b3be28849ecb16
Anggomez / Proyecto_Knight <angeltaustez@gmail.com>
2020-11-11 22:08:02 +0100

    Menu

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+//using System.Runtime.Hosting;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void cargarEscena()
+    {
+       // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
+    }
+    public void cerrarJuego()
+    {
+        Application.Quit();
+    }
+}
